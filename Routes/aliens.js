4a40fb7794c9c600eb501710802c5143c889801e
Pranay Dange <pranaydange04@gmail.com>
2022-12-27 20:30:37 +0530

    updated model,schema

--- Routes/aliens.js
+++ Routes/aliens.js
@@ -1,9 +1,18 @@
 const express=require('express');
+const schema_obj = require('../models/elements_schema');
+
 
 const router=express.Router();
 
-router.get('/', ()=>{
-    console.log('in route folder and get is used')
+router.get('/', async (req,res)=>{
+    try{
+
+        const aliens=await schema_obj.find();
+        res.json(aliens);
+
+    }catch(err){
+        res.send("Error"+err)
+    }
 })
 
 module.exports=router;
